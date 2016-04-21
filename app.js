8771a9037b61d248c51cf6ce05559f3c4475cc83
alu0100836059 <alu0100836059@ull.edu.es>
2016-04-21 01:01:56 +0000

    Iniciando mongoDB

--- app.js
+++ app.js
@@ -5,7 +5,18 @@ const app = express();
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
 
-app.set('port', (process.env.PORT || 5000));
+var pmongo = require("promised-mongo");
+var db = pmongo(connectionString, [collections]);
+
+
+var port = process.env.PORT || 3000;
+var ip = process.env.IP || '0.0.0.0';
+var addr = `${ip}:${port}`;
+
+var host = db.serverStatus().host;
+var prompt = function(){ return db+"@"+host+">";}
+
+//app.set('port', (process.env.PORT || 5000));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -25,6 +36,9 @@ app.get('/csv', (request, response) => {
     response.send({ "rows": calculate(request.query.input) });
 });
 
-app.listen(app.get('port'), () => {
-    console.log(`Node app is running at localhost: ${app.get('port')}` );
+//app.listen(app.get('port'), () => {
+//    console.log(`Node app is running at localhost: ${app.get('port')}` );
+
+app.listen(port,ip,function(){
+    console.log(`chat server listening at ${addr}`);
 });
